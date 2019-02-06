40aa047db978a99512d2dbc657fd112d12c14a87
Viktor Villainov <supervillain@riseup.net>
2019-02-06 14:41:29 -0500

    bump version number

--- kote/__version__.py
+++ kote/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'kote'
 __description__ = 'Invisible Messaging'
 __url__ = 'https://github.com/l-n-s/kote'
-__version__ = '1.1'
+__version__ = '1.2'
 __author__ = 'Viktor Villainov'
 __author_email__ = 'supervillain@riseup.net'
 __license__ = 'MIT'
